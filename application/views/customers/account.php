51413be131a62a1c82e97e8ee40699002318afb2
Joey Carmello <Joeycarmello@gmail.com>
2011-10-19 23:26:50 -0400

    Whoops, mised those files last time

---
+++ application/views/customers/account.php
@@ -0,0 +1,17 @@
+<div class="page-header">
+  <h2>Account <small>If you cannot find anything, do not hesitate to contact <a href="/statics/help">help</a></small></h2>
+</div>
+
+<div class="container">
+  <section>
+    <div class="page-header">
+      <h3>Your Active Orders</h3>
+    </div>
+    <div class="page-header">
+      <h3>Your Cart</h3>
+    </div>
+    <div class="page-header">
+      <h3>Your Prevous Orders</h3>
+    </div>
+  </section>
+</div>
