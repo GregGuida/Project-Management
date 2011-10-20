cacb3e753c436e16831573f2c05487f95f94279e
Sean Dunn <sean@attamusc.com>
2011-10-20 18:35:22 -0400

    For working php fog app

--- application/views/statics/help.php
+++ application/views/statics/help.php
@@ -4,6 +4,6 @@
 
 <ul id="help-faq-list" class="unstyled">
   <li>
-    <h4>I forgot my password <small>You can reset your password by entering your email address on our <a href="/customers/reset_password">forgot my password</a> page</small></h4>
+    <h4>I forgot my password <small>You can reset your password by entering your email address on our <a href="/customers/forgot_password">forgot my password</a> page</small></h4>
   </li>
 </ul>
