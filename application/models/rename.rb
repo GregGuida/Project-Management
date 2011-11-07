42974434dd1ef9bb93faf7d2ebd5aa13d9d7288f
alec dulan <alec.dulan@gmail.com>
2011-11-07 17:00:54 -0500

    rename fix 2

--- application/models/rename.rb
+++ application/models/rename.rb
@@ -2,7 +2,7 @@
 
 Dir.foreach('.') do |d|
   if File.exists?(d) and d != '.' and d != '..'
-    File.rename("#{d}", "#{d}what")
-    #File.rename("#{d}", "#{d}".sub(/what/, ''))
+    #File.rename("#{d}", "#{d}what")
+    File.rename("#{d}", "#{d}".sub(/what/, ''))
   end
 end
