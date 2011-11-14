ae2afabeed277c8f92daea8333efe978009ea1f8
alec dulan <alec.dulan@gmail.com>
2011-11-14 00:24:51 -0500

    Created branch

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -1,9 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Cart extends CI_Controller {
+
   public $layout = 'main';
+
+  public function __construct()
+  {
+    parent::__construct();
+  } 
 
   public function index() {
     $this->load->view('cart/show');
   }
+  
 }
+
+php>
