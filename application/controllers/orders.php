ab3678b23d2d98f2ec6b1da72735c605bf4f078b
Sean Dunn <sean@attamusc.com>
2011-11-15 01:09:16 -0500

    Orders Model almost finished. Began Orders Unit Test

--- application/controllers/orders.php
+++ application/controllers/orders.php
@@ -28,5 +28,18 @@ class Orders extends CI_Controller {
     $this->layout = "admin";
     $this->load->view('orders/admin_show');
   }
+  
+  public function test_run() {
+    $this->load->library('unit_test');
+    $this->load->model('Order', 'order');
+
+    //Run some tests against the Fixture Data.
+    $this->unit->run($this->order->convert_cart_to_order(1, 1), true, 'Orders convert_cart_to_order() Return Type Test', 'Make sure that convert_cart_to_order() doesn\'t fail.');
+    
+    //Pass a report to the view
+    $data['test_result'] = $this->unit->report();
+          
+    $this->load->view('test_runner', $data);
+  }
 
 }
--- application/models/order.php
+++ application/models/order.php
@@ -29,9 +29,13 @@ class Order extends CI_Model {
 	 * @return array $order the associative array of the order, or false if it doesn't exist
 	 */
 	function find($order_num) {
-	    var $order = false;
-	    
-	    // Some stuff...
+	    $order = false;
+	    $cursor = $this->db->get_where('Orders', array('OrderNum' => $order_num));
+        
+        if($cursor->num_rows() > 0) {
+            $order = $cursor->row_array();
+            $cursor->free_result();
+        }
 	    
 	    return $order;
 	}
@@ -44,9 +48,12 @@ class Order extends CI_Model {
 	 * @return array $orders the array of orders filling the criteria
 	 */
 	function find_by($data) {
-	    var $orders = array();
-	    
-	    // Some stuff...
+	    $orders = array();
+	    $cursor = $this->db->get_where('Orders', $data);
+        
+        foreach($cursor->result_array() as $order) {
+            $orders[] = $order;
+        }
 	    
 	    return $orders;
 	}
@@ -59,9 +66,12 @@ class Order extends CI_Model {
 	 * @return array $orders the array of orders
 	 */
 	function all($limit = 0) {
-	    var $orders = array();
-	    
-	    // Some stuff...
+	    $orders = array();
+	    $cursor = $this->db->get('Orders', $limit);
+
+        foreach($cursor->result_array() as $order) {
+            $orders[] = $order;
+        }
 	    
 	    return $orders;
 	}
@@ -74,37 +84,46 @@ class Order extends CI_Model {
 	 * @return int $order_num the id of the created object
 	 */
 	function create($data) {	    
-	    // Some stuff...
-	    
+	    $order_num = false;
+        $this->db->insert('Orders', $data);
+        
+        if(!$this->db->_error_message()) {
+            $order_num = $this->db->insert_id();
+        }
+
 	    return $order_num;
 	}
 	
 	/*
 	 * Updates an order based on the passed in order id
 	 *
-	 * @param int $order_num id of the order to update
+	 * @param int   $order_num  id of the order to update
+	 * @param array $data       the associative array of the data to be updated
 	 *
-	 * @return boolean $success if the update was successful or not.
+	 * @return int $result id of the updated item, or false.
 	 */
-	function update($order_num) {
-	    var $success = false;
-	    	    
-	    // Some stuff...
-	    
-	    return $success;
+	function update($order_num, $data) {
+	    $result = false;
+	    $this->db->update('Orders', $data, array('OrderNum' => $order_num));
+
+        if (!$this->db->_error_message()) {
+          $result = $order_num;
+        }
+	    return $result;
 	}
 	
 	/*
-	 * Deletes an order based on the passed in order id
+	 * Deletes an order based on the passed in order id, as well as all items in that order.
+	 * THIS SHOULD NEVER EVER BE CALLED! Here for completions sake.
 	 *
 	 * @param array $data associative array of the order to create
 	 *
 	 * @return int $order_num the id of the created object
 	 */
 	function delete($order_num) {	    
-	    // Some stuff...
-	    
-	    return $order_num;
+	    $this->db->delete('Orders', array('OrderNum' => $order_num));
+	    $this->db->delete('OrderedItems', array('OrderNum' => $order_num));
+	    return !!$this->db->_error_message();
 	}
 	
 	// ---------Convenience Functions---------
@@ -116,8 +135,44 @@ class Order extends CI_Model {
 	 *
 	 * @return boolean $success if the conversion was successful or not
 	 */
-	function convert_cart_to_order() {
-	    var $success = false;
+	function convert_cart_to_order($uid, $sid) {
+	    // Load Model and Date Helper
+	    $this->load->model("Ordered_Item", "item");
+	    //TO-DO: Incorporate the CartItems Model
+	    //$this->load->model("CartItem", "cart_item");
+	    $this->load->helper("date");
+
+	    $success = false;
+	    $ordered_items = array();
+	    
+	    // Get the Total Price of all items in the given order
+	    $totalPriceUSD = current($this->db->select("SUM(products.priceUSD)")->from("products")->join("stockitems", "products.pid = stockitems.pid")->join("cartitems", "cartitems.stockID = stockitems.stockID")->where("cartitems.uid", $uid)->where("didPurchase = 0")->get()->row_array());
+	    
+	    // Get all the user's unpurchased cart items
+	    $stock_items_cursor = $this->db->get_where("CartItems", array("uid" => $uid, "didPurchase" => 0));
+	    
+	    // Insert the Order
+	    $order_num = $this->create(array(
+	                        "uid" => $uid, 
+            	            "sid" => $sid, 
+            	            "date" => date('Y-m-d H:i:s', now()),
+            	            "status" => "Processing",
+            	            "totalpriceusd" => $totalPriceUSD));
+        
+        // Create and insert the various Ordered Items
+        foreach($stock_items_cursor->result_array() as $stock_item) {
+            $ordered_items[] = array("OrderNum" => $order_num, "uid" => $uid, "stockID" => $stock_item["stockID"]);
+        }
+        
+        // Set the 'didPurchase' flag in the CartItems table to 1 for the user.
+        // $this->cart_item->update(array("uid" => $uid), array("didPurchase" => 1));
+        
+        $items_creation_success = $this->item->create($ordered_items);
+        
+        // Check for error messages
+        if(!$this->db->_error_message() && $items_creation_success) {
+            $success = true;
+        }
 	    
 	    return $success;
 	}
@@ -127,10 +182,10 @@ class Order extends CI_Model {
 	 *
 	 * @param int $order_num the id of the concerned order
 	 *
-	 * @return array $product_ids array of the product ids associated with a given order
+	 * @return array $product_ids array of the products (with pid, name, image[0], and price) associated with a given order
 	 */
 	function get_products_in_order($order_num) {
-	    var $product_ids = array();
+	    $product_ids = array();
 	    
 	    return $product_ids;
 	}
