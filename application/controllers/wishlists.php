274ad2ae94e9a4d8b9267494e5b68dcb27396c54
alec dulan <alec.dulan@gmail.com>
2011-11-15 14:10:57 -0500

    WishList model complete - All tests pass.

--- application/controllers/wishlists.php
+++ application/controllers/wishlists.php
@@ -13,14 +13,16 @@ class WishLists extends CI_Controller {
   
   public function test_run() {
       $this->load->library('unit_test');
-      $this->load->model('wishLists', 'item');
+      $this->load->model('WishList', 'item');
       
-      $date1 = date( 'Y-m-d H:i:s',mktime(0,17,35,11,15,2011));
+      $date1 = date('Y-m-d H:i:s',mktime(0,17,35,11,15,2011));
       //Run some tests against the Fixture Data.
-      $this->unit->run($this->item->getCart(1), 'is_array', 'CartItems getCart() General Test', 'Make sure that getCart(1) returns an array.');
-      $this->unit->run($this->item->getCart(1), array(array('uid' => 1, 'stockID' => 1, 'dateAdded' => $date1,'didPurchase' => 0)), 'CartItems getCart(1) Test', 'Make sure that the value of using getCart(1) returns the data we expect from the database.');
-      $this->unit->run(current($this->item->getAllCarts(100)), array('uid' => 1, 'stockID' => 1, 'dateAdded' => $date1,'didPurchase' => 0), 'Ordered Item all() test', 'Make sure that the value of using all() returns the data we expect from the database.');
-      $this->unit->run($this->item->addItemToCart(1,1), true, 'Ordered Item create() General Test', 'Make sure that create() returns true and that it works for batches.');
+      $this->unit->run($this->item->getWishList(1), 'is_array', 'Wish Lists get() General Test', 'Make sure that getWishList(1) returns an array.');
+      $this->unit->run($this->item->getWishList(1), array('wishID' => 1, 'uid' => 1, 'name' => 'Test List',array('pid'=>1)), 'Wish Lists get() Test', 'Make sure that the value of using getWishList(1) returns the data we expect from the database.');
+      $this->unit->run($this->item->addItemToList(2,1), true, 'Wish List addItem() General Test', 'Make sure that addItemToList() returns true.');
+      $this->unit->run($this->item->removeItemFromList(2,1), true, 'Wish List removeItem() General Test', 'Make sure that removeItemFromList() returns true.');
+      $this->unit->run($this->item->newWishList(1,'Second list'), true, 'New Wish List Test', 'Make sure that newWishList() returns true.');
+      $this->unit->run($this->item->deleteWishList(2), true, 'Delete Wish List Test', 'Make sure deleteWishList() returns true.');
       
       //Pass a report to the view
       $data['test_result'] = $this->unit->report();
