b8520907516fb743f5138511366f3358046d4b1d
Gregory Guida <gregory.guida1@gmail.com>
2011-11-18 16:57:39 -0500

    added a few models commiting changes

---
+++ application/models/comments.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ *
+ * DROP TABLE IF EXISTS `CodeIgniter2`.`Comments`;
+ * CREATE TABLE `CodeIgniter2`.`Comments` (
+ *   `comID` INT NOT NULL AUTO_INCREMENT,
+ *   `uid` INT NOT NULL REFERENCES `CodeIgniter2`.`Users` (`uid`),
+ *   `pid` INT NOT NULL REFERENCES `CodeIgniter2`.`Products` (`pid`),
+ *   `Remark` VARCHAR( 250 ) NOT NULL ,
+ *   `Date` TIMESTAMP NOT NULL ,
+ *   `Rating` DECIMAL NOT NULL,
+ *   PRIMARY KEY ( `comID` ) ,
+ *   INDEX ( `uid` , `pid` )
+ * ) ENGINE = INNODB;
+ *
+ */
+
+class Comments extends CI_Model {
+	
+  //Constructor
+  function __construct() {
+    parent::__construct();
+  }
+	
+  //Returns the Comment with the given id# as an array
+  function find($id) {
+    $data = array();
+
+    $this->db->select('*');	
+    $this->db->from('Comments');
+    $this->db->join('Users','User.uid = Comments.uid');
+    $query = $this->db->get();
+    
+    if( $query->num_rows() == 0 ) {
+      return false;
+    }
+    
+    $data = $query->row_array();
+ 
+    $query->free_result(); //Release our memory
+    return $data;
+  }
+
+
+  // accepts an optional limit
+  // and returns an array of user row arrays
+  function all($limit = 0, orderBy = null, direction = "desc") {
+    $comments = array();
+
+    if (orderBy != null){
+    	$query = $this->db->order_by(orderBy, direction);
+    }
+
+    $query = $this->db->get('Comments', $limit);
+
+    foreach($query->result_array() as $comment) {
+      $comments[] = $comment
+    }
+
+    $cursor->free_result();
+    return $comments;
+  }
+	
+  function create($name,$description,$price,$catID) {
+    $data = array(
+      'Name' => $name,
+      'Description' => $description,
+      'PriceUSD' => $price,
+      'catID' => $catID
+      );
+    $this->db->insert('Comments', $data);
+
+    return $this->db->insert_id();
+  }
+	
+  function destroy($id) {
+    $this->db->delete('Comments', array('pid' => $id));
+    return true;
+  }
+	
+  function update($id,$name,$description,$price,$catID){
+    $data = array(
+      'Name' => $name,
+      'Description' => $description,				
+      'PriceUSD' => $price,
+      'catID' => $catID
+    );
+    $this->db->update('Comments', $data, array('pid' => $id));
+    return true;
+  }
+	
+}
+
+?>
