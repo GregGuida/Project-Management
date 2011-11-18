9f95e5bd8383a45e669aaf68ebd700ea18302ebe
Sean Dunn <sean@attamusc.com>
2011-11-18 08:54:45 -0500

    Test Names are now correct

--- application/controllers/stock_tickets.php
+++ application/controllers/stock_tickets.php
@@ -23,9 +23,9 @@ class Stock_tickets extends CI_Controller {
       $this->load->model("Stock_Ticket", "ticket");
       
       $this->unit->run($this->ticket->find(1), array("ticketNum"=>"1", "pid"=>"1", "uid"=>"2", "PriceUSD"=>"10", "NumOrdered"=>"2", "DateSubmitted"=>"2011-11-18 00:23:35", "Status"=>"Delivered"), 'Stock Ticket find(1) General Test', 'Test that the find(1) function of the Stock Ticket Model returns the correct object.');
-      $this->unit->run($this->ticket->find(0), false, 'Stock Ticket find(0) General Test', 'Test that the find(0) function of the Stock Ticket Model returns \'false\' if the ticket doesn\'t exist.');
-      $this->unit->run(count($this->ticket->all(5)), 5, 'Stock Ticket all(5) General Test', 'Test that the all function returns the correct limit specified.');
-      $this->unit->run(current($this->ticket->all(1)), array("ticketNum"=>"1", "pid"=>"1", "uid"=>"2", "PriceUSD"=>"10", "NumOrdered"=>"2", "DateSubmitted"=>"2011-11-18 00:23:35", "Status"=>"Delivered"), 'Stock Ticket find(0) General Test', 'Test that the all(1) function of the Stock Ticket Model returns the correct database row.');
+      $this->unit->run($this->ticket->find(0), false, 'Stock Ticket find(0) Return Type Test', 'Test that the find(0) function of the Stock Ticket Model returns \'false\' if the ticket doesn\'t exist.');
+      $this->unit->run(count($this->ticket->all(5)), 5, 'Stock Ticket all(5) Limit Test', 'Test that the all function returns the correct limit specified.');
+      $this->unit->run(current($this->ticket->all(1)), array("ticketNum"=>"1", "pid"=>"1", "uid"=>"2", "PriceUSD"=>"10", "NumOrdered"=>"2", "DateSubmitted"=>"2011-11-18 00:23:35", "Status"=>"Delivered"), 'Stock Ticket all(1) General Test', 'Test that the all(1) function of the Stock Ticket Model returns the correct database row.');
       
       //Pass a report to the view
       $data['test_result'] = $this->unit->report();
