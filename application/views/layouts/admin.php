9d8329c62196a9b468dcd8653cf768b3a5542df0
Sean Dunn <sean@attamusc.com>
2011-11-21 16:16:18 -0500

    Whoops

--- application/views/layouts/admin.php
+++ application/views/layouts/admin.php
@@ -75,7 +75,7 @@
       <a href="#" class="dropdown-toggle">Stock</a>
       <ul class="dropdown-menu">
         <li><a href="/stock_tickets/admin_browse">View Stock</a></li>
-        <li><a href="/stock_tickets/admin_add">Add Stock</a></li>
+        <li><a href="/stock_tickets/add">Add Stock</a></li>
       </ul>
     </li>
 	    </ul>
