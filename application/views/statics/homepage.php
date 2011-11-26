060b55f284bab543fafd1fae4eebb9691c37e66f
krishnan kottaiswamy <kriish30@gmail.com>
2011-11-26 09:24:15 -0500

    Product table is empty

--- application/views/statics/homepage.php
+++ application/views/statics/homepage.php
@@ -25,12 +25,12 @@
             echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
         }
     ?>
-        <button id="new-image" class="btn"> Add An <br/>Image </butto</a></p><span class="help-block"><b><?= $products[0]['Name'] ?></b></span></div>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[0]['Name'] ?></b></span></div>
 		
 		<div class="span3"><p><a href="/products/show/">
 	<?
       if(!empty($images)) {
-        echo '<img src="'.$images[0]['location'].'" style="width:340px;" class="product-image" />';
+        echo '<img src="'.$images[1]['location'].'" style="width:340px;" class="product-image" />';
         }
 	?>
     </p>
@@ -40,12 +40,12 @@
             echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
         }
     ?>
-        <button id="new-image" class="btn"> Add An <br/>Image </butto</a></p><span class="help-block"><b><?= $products[0]['Name'] ?></b></span></div>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[1]['Name'] ?></b></span></div>
 		
 		<div class="span3"><p><a href="/products/show/">
 	<?
       if(!empty($images)) {
-        echo '<img src="'.$images[0]['location'].'" style="width:340px;" class="product-image" />';
+        echo '<img src="'.$images[2]['location'].'" style="width:340px;" class="product-image" />';
         }
 	?>
     </p>
@@ -58,13 +58,13 @@
         <button id="new-image" class="btn"> Add An <br/>Image </button>
 	</a>
 	</p>
-	<span class="help-block"><b><?= $products[0]['Name'] ?></b></span>
+	<span class="help-block"><b><?= $products[2]['Name'] ?></b></span>
 	</div>
 		
 	<div class="span3"><p><a href="/products/show/">
 	<?
       if(!empty($images)) {
-        echo '<img src="'.$images[0]['location'].'" style="width:340px;" class="product-image" />';
+        echo '<img src="'.$images[3]['location'].'" style="width:340px;" class="product-image" />';
         }
 	?>
     </p>
@@ -74,12 +74,12 @@
             echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
         }
     ?>
-        <button id="new-image" class="btn"> Add An <br/>Image </butto</a></p><span class="help-block"><b><?= $products[0]['Name'] ?></b></span></div>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[3]['Name'] ?></b></span></div>
 		
 		<div class="span3"><p><a href="/products/show/">
 	<?
       if(!empty($images)) {
-        echo '<img src="'.$images[0]['location'].'" style="width:340px;" class="product-image" />';
+        echo '<img src="'.$images[4]['location'].'" style="width:340px;" class="product-image" />';
         }
 	?>
     </p>
@@ -89,23 +89,182 @@
             echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
         }
     ?>
-        <button id="new-image" class="btn"> Add An <br/>Image </butto</a></p>
-		<span class="help-block"><b><?= $products[0]['Name'] ?></b></span>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p>
+		<span class="help-block"><b><?= $products[4]['Name'] ?></b></span>
 		</div>
 		
     </div>
-    <div class="row">
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-  </div>
-  <div class="row">
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-    <div class="span3"><p><a href="/products/show/"><img src="http://placekitten.com/120/120" class="thumbnail" /></a></p><span class="help-block"><b>A Cute Kitty</b></span></div>
-  </div>
+    
+	<div class="row">
+    <div class="span3">
+	<p>
+	<a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[5]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[5]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[6]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[6]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[7]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button>
+	</a>
+	</p>
+	<span class="help-block"><b><?= $products[7]['Name'] ?></b></span>
+	</div>
+		
+	<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[8]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[8]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[9]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p>
+		<span class="help-block"><b><?= $products[9]['Name'] ?></b></span>
+		</div>
+		
+    </div>
+	
+	<div class="row">
+    <div class="span3">
+	<p>
+	<a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[10]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button>
+		</a></p><span class="help-block"><b><?= $products[10]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[11]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[11]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[12]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button>
+	</a>
+	</p>
+	<span class="help-block"><b><?= $products[12]['Name'] ?></b></span>
+	</div>
+		
+	<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[13]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p><span class="help-block"><b><?= $products[13]['Name'] ?></b></span></div>
+		
+		<div class="span3"><p><a href="/products/show/">
+	<?
+      if(!empty($images)) {
+        echo '<img src="'.$images[14]['location'].'" style="width:340px;" class="product-image" />';
+        }
+	?>
+    </p>
+    <p class="alt-images">
+    <?
+        foreach( $images as $preview_image) {
+            echo '<img src="'.$preview_image['location'].'" style="width:60px;height:60px;class="thumbnail"/>';
+        }
+    ?>
+        <button id="new-image" class="btn"> Add An <br/>Image </button></a></p>
+		<span class="help-block"><b><?= $products[14]['Name'] ?></b></span>
+		</div>
+		
+    </div>
 </section>
