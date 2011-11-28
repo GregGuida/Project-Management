3671203989c333e5a75b55269d423c904ad7743e
Joey Carmello <Joeycarmello@gmail.com>
2011-11-28 15:10:14 -0500

    nivo slider pulls from products now

--- application/views/statics/homepage.php
+++ application/views/statics/homepage.php
@@ -1,10 +1,8 @@
 <section class="product-slider">
   <div id="main-slider" class="nivoSlider theme-default" style="width:940px;height:400px">
-    <img src="http://flickholdr.com/940/400/shoes" title="Buy One Get One Half-Off Shoes"/>
-    <img src="http://flickholdr.com/940/400/books/bw" title="%50 off all books"/>
-    <img src="http://flickholdr.com/940/400/toys" title="all new selection of toys" />
-    <img src="http://flickholdr.com/940/400/toys,games,animals,fun,product/bw" title="whatever this is... we've got it"/>
-    <img src="http://flickholdr.com/940/400/toys,games,animals,products,fun" title="I dont always put captions on my slider images... oh wait, I do"/>
+    <?php for ($i = 0; $i < count($products); $i++) { ?>
+      <img src="<?php echo $products[$i]['location'] ?>" title="<?php echo $products[$i]['Description'] ?>" class="homepage_slider_image" />
+    <?php } ?>
   </div>
 </section>
 
