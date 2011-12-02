1c5908d49059c5c38e1b7a27c2cf8bf96087f571
Sean Dunn <sean@attamusc.com>
2011-12-01 22:53:48 -0500

    Baseline of new wishlist items

---
+++ application/models/wish_list_item.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ *	CREATE TABLE `CodeIgniter2`.`WishListItems` (
+ *		`wishID` INT NOT NULL REFERENCES `CodeIgniter2`.`WishLists` (`wishID`),
+ *		`pid` INT NOT NULL REFERENCES `CodeIgniter2`.`Products` (`pid`),
+ *	PRIMARY KEY ( `wishID` , `pid` )
+ *	) ENGINE = INNODB;
+*/
+
+class Wish_List extends CI_Model
+{
+	function __construct(){
+		parent::__construct();
+		// Custom constructor code goes here...
+	}
+	
+	function find($order_num) {
+	    $order = false;
+	    $cursor = $this->db->get_where('Orders', array('OrderNum' => $order_num));
+        
+        if($cursor->num_rows() > 0) {
+            $order = $cursor->row_array();
+            $cursor->free_result();
+        }
+	    
+	    return $order;
+	}
+	
+	function find_by($data) {
+	    $orders = array();
+	    $cursor = $this->db->get_where('Orders', $data);
+        
+        foreach($cursor->result_array() as $order) {
+            $orders[] = $order;
+        }
+	    
+	    return $orders;
+	}
+	
+	function all($limit = 0) {
+	    $orders = array();
+	    $cursor = $this->db->get('Orders', $limit);
+
+        foreach($cursor->result_array() as $order) {
+            $orders[] = $order;
+        }
+	    
+	    return $orders;
+	}
+	
+	function create($data) {	    
+	    $order_num = false;
+        $this->db->insert('Orders', $data);
+        
+        if(!$this->db->_error_message()) {
+            $order_num = $this->db->insert_id();
+        }
+
+	    return $order_num;
+	}
+	
+	function update($order_num, $data) {
+	    $result = false;
+	    $this->db->update('Orders', $data, array('OrderNum' => $order_num));
+
+        if (!$this->db->_error_message()) {
+          $result = $order_num;
+        }
+	    return $result;
+	}
+	
+	function delete($order_num) {	    
+	    $this->db->delete('Orders', array('OrderNum' => $order_num));
+	    $this->db->delete('OrderedItems', array('OrderNum' => $order_num));
+	    return !!$this->db->_error_message();
+	}
+}
+}
